fdcdf79975be3a4ba2a5c7e4841fe097ed8b3cc4
Jeff Handley <jeff.handley@concur.com>
2016-07-20 13:38:25 -0700

    Use a functional component for the Basic demo

--- demo/Basic.jsx
+++ demo/Basic.jsx
@@ -1,39 +1,33 @@
 import React from 'react';
 import Table, { alternateRows } from '../src';
 
-export default React.createClass({
-    displayName: 'Basic Demo',
-
-    render() {
-        return (
-            <Table>
-                <Table.Head>
-                    <Table.HeadRow>
-                        <Table.HeadCell>Name</Table.HeadCell>
-                        <Table.HeadCell>Allowance</Table.HeadCell>
-                    </Table.HeadRow>
-                </Table.Head>
-                <Table.Body>
-                    <Table.BodyRow>
-                        <Table.BodyCell>Ed</Table.BodyCell>
-                        <Table.BodyCell>$75.00</Table.BodyCell>
-                    </Table.BodyRow>
-                    <Table.BodyRow>
-                        <Table.BodyCell>Matt</Table.BodyCell>
-                        <Table.BodyCell>$100</Table.BodyCell>
-                    </Table.BodyRow>
-                    <Table.BodyRow>
-                        <Table.BodyCell>Jeff</Table.BodyCell>
-                        <Table.BodyCell>$125.00</Table.BodyCell>
-                    </Table.BodyRow>
-                </Table.Body>
-                <Table.Foot>
-                    <Table.FootRow>
-                        <Table.FootCell>Total</Table.FootCell>
-                        <Table.FootCell>$300.00</Table.FootCell>
-                    </Table.FootRow>
-                </Table.Foot>
-            </Table>
-        );
-    }
-});
+export default () => (
+    <Table>
+        <Table.Head>
+            <Table.HeadRow>
+                <Table.HeadCell>Name</Table.HeadCell>
+                <Table.HeadCell>Allowance</Table.HeadCell>
+            </Table.HeadRow>
+        </Table.Head>
+        <Table.Body>
+            <Table.BodyRow>
+                <Table.BodyCell>Ed</Table.BodyCell>
+                <Table.BodyCell>$75.00</Table.BodyCell>
+            </Table.BodyRow>
+            <Table.BodyRow>
+                <Table.BodyCell>Matt</Table.BodyCell>
+                <Table.BodyCell>$100</Table.BodyCell>
+            </Table.BodyRow>
+            <Table.BodyRow>
+                <Table.BodyCell>Jeff</Table.BodyCell>
+                <Table.BodyCell>$125.00</Table.BodyCell>
+            </Table.BodyRow>
+        </Table.Body>
+        <Table.Foot>
+            <Table.FootRow>
+                <Table.FootCell>Total</Table.FootCell>
+                <Table.FootCell>$300.00</Table.FootCell>
+            </Table.FootRow>
+        </Table.Foot>
+    </Table>
+);
